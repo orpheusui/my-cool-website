d51f77c3be7c2498a2a62d772a1070bf2445b368
Derek Olsen <derek@neutrino-bomb.myfiosgateway.com>
2022-03-03 18:13:45 -0500

    passphrase generator

--- correcthorse/correcthorsebatterystaple.js
+++ correcthorse/correcthorsebatterystaple.js
@@ -1,22 +1,30 @@
-// 7775 words
-
 function horse() {
 
-var count = document.getElementById("length").value
-var capitalization
-var separator = document.getElementById("separator").value
-var password = ""
+    var count = document.getElementById("length").value
+    var capitals = document.getElementById("capitals").checked
+    var separator = document.getElementById("separator").value
+    var password = ""
+    var newword
 
 
+    while (count != 0) {
 
-while (count != 0) {
-    password += largeWordlist[Math.floor(Math.random() * largeWordlist.length)] + separator
-    count -= 1
-}
+        newword = largeWordlist[Math.floor(Math.random() * largeWordlist.length)]
 
-if (separator != "") {
-    password = password.slice(0, password.length - 1)
-}
+        if (capitals) {
+            password += newword.charAt(0).toUpperCase() + newword.slice(1) + separator
+        } else {
+            password += newword + separator
+        }
 
-return password;
+        count -= 1
+    }
+
+    if (separator != "") {
+        password = password.slice(0, password.length - 1)
+    }
+
+    document.getElementById("outputLength").innerHTML = "Password length: " + password.length
+
+    return password;
 }
